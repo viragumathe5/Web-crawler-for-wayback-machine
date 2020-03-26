739aef5a0554999119536b26d8d03914c5b5b7b5
Christian Clauss <cclauss@me.com>
2020-03-26 18:41:04 +0100

    Add get_banner() and remove debug code

--- crawler.py
+++ crawler.py
@@ -3,19 +3,20 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
 
+def get_banner() -> str:
+    return f"\n<{'*' * 104}>\n"
+
+
 def get_soup_from_url(url: str) -> BeautifulSoup:
     with urlopen(url) as reply:
         return BeautifulSoup(reply.read())
 
 
 if __name__ == "__main__":
+    banner = get_banner()
     soup = get_soup_from_url("https://www.pixar.com/feature-films/cars-3")
-    print(soup)
-    print(f"\n<{'*' * 104}>\n")
     links = soup.find_all("img")
-    print(f"\n<{'*' * 104}>\n")
-    print(links)
-    print(f"\n<{'*' * 104}>\n")
+    print(banner)
     sources = [src for img in soup.find_all("img") if (src := img.get("src"))]
     f"{len(sources) / len(links) * 100:.2f}% of imgs have a src attribute."
     for i, src in enumerate(sources, 1):
