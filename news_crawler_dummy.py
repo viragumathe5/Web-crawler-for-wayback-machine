7d9a43572fe0cc613494f92540ab91616fcadab3
Christian Clauss <cclauss@me.com>
2020-03-26 18:45:01 +0100

    from .crawler import get_banner, get_soup_from_url

---
+++ news_crawler_dummy.py
@@ -0,0 +1,10 @@
+from .crawler import get_banner, get_soup_from_url
+
+if __name__ == "__main__":
+    banner = get_banner()
+    soup = get_soup_from_url("https://www.washingtonpost.com/us-policy/2020/03/24/"
+                             "trump-coronavirus-congress-economic-stimulus/")
+    print(banner)
+    headline = soup.find('h1', {"class": "font--headline gray-darkest mb-sm null"})
+    print(headline.get_text())
+    print(banner)
